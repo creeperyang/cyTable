846d1957ccf72a42bf49b6a1e0aed56fa62c7f6c
creeperyang <yangcreeper@hotmail.com>
2015-08-27 14:47:57 +0800

    add coverage test

--- karma.conf.js
+++ karma.conf.js
@@ -35,14 +35,21 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'src/*.js': 'coverage'
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
+        // Reporter Options
+        coverageReporter: {
+            type : 'lcov',
+            dir : 'coverage/'
+        },
 
         // web server port
         port: 9876,
@@ -63,7 +70,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['Chrome', 'Firefox'],
 
 
         // Continuous Integration mode
